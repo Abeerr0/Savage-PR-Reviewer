88b89dee4f24441aa36d1da960dec7c50c75207c
Abeerr0 <abeer.ali.alsaiari1@gmail.com>
2026-06-05 14:44:12 +0000

    Add messy code to test the Savage Bot

---
+++ bad_code.py
@@ -0,0 +1,11 @@
+def calculate_stuff(a, b):
+    x = a + b
+    print("the result is")
+    return x
+    print("done calculating") # سطر كارثي مستحيل يشتغل
+    
+def useless_function():
+    my_list = [1, 2, 3]
+    for item in my_list:
+        pass
+        pass
